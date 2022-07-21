64077fa550365def63616c16650a067e1c6fdf0a
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-07-21 20:26:16 +0100

    Converted code to Python 3

--- fastnetmon_api_client.py
+++ fastnetmon_api_client.py
@@ -3,7 +3,7 @@
 import requests
 import os
 import sys
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import random
 
 # Auth details
@@ -12,32 +12,32 @@ auth_data = ('admin', 'your_password_replace_it')
 # Create random network for tests
 new_network = "%d.%d.%d.%d/22" % (random.randint(1,254), random.randint(1,254), random.randint(1,254), random.randint(1,254))  
 
-print "Add random network:", new_network
+print("Add random network:", new_network)
 
 # Encode any values which have spaces or '/' inside them
-new_network_encoded = urllib.quote_plus(new_network)
+new_network_encoded = urllib.parse.quote_plus(new_network)
 
 # Create networks_list entry
 r = requests.put('http://127.0.0.1:10007/main/networks_list/' + new_network_encoded, auth=auth_data)
 
 if r.status_code == 200:
-    print "Correctly added new subnet"
+    print("Correctly added new subnet")
 else:
-    print "Can't add new subnet", r.json()['error_text']
+    print("Can't add new subnet", r.json()['error_text'])
 
 # Show all networks list entries
 r = requests.get('http://127.0.0.1:10007/main/networks_list', auth=auth_data)
 
 if r.status_code != 200:
-    print "API call failed"
+    print("API call failed")
     sys.exit(0)
 
-print "Networks list with new network:", r.json()
+print("Networks list with new network:", r.json())
 r = requests.delete('http://127.0.0.1:10007/main/networks_list/' + new_network_encoded, auth=auth_data)
 
 if r.status_code != 200:
-    print "Cannot delete network from networks_list"
+    print("Cannot delete network from networks_list")
     sys.exit(0)
 else:
-    print "Correctly removed new subnet"
+    print("Correctly removed new subnet")
     sys.exit(0)
